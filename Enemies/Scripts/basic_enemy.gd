9496a0f1b3c97de9a8341d707695f492a636840f
Daikaigan <bravenhebl@gmail.com>
2024-09-10 19:39:32 +0800

    Enemies rotate to Player

--- Enemies/Scripts/basic_enemy.gd
+++ Enemies/Scripts/basic_enemy.gd
@@ -6,10 +6,6 @@ var m_gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export var money_amount : int
 @export var money_value : int 
 
-func _ready() -> void:
-	await get_tree().create_timer(5).timeout
-	_on_health_manager_on_death()
-
 func _physics_process(delta: float) -> void:
 	var m_position_diff = Global.player_coords - position 
 	m_position_diff.y = 0
@@ -20,6 +16,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= m_gravity * delta
 	
+	rotation.y = - Vector2(m_position_diff.x, m_position_diff.z).angle()
 	move_and_slide()
 
 func _on_health_manager_on_death() -> void:
